5b7586cf03450f6fea3aa0021a3fc73f88ae738f
Chris Padilla <cdpadilla42@gmail.com>
2020-04-01 13:25:23 -0500

    Write out controller placeholders

---
+++ controllers/movieInstranceController.js
@@ -0,0 +1,41 @@
+var movieInstance = require('../models/movie');
+
+// Display list of all MovieInstrances
+exports.movieInstance_list = (req, res) => {
+  res.send('NOT IMPLEMENTED: Movie Instance List');
+};
+
+// Display details page for MovieInstrances
+exports.movieInstance_detail = (req, res) => {
+  res.send('NOT IMPLEMENTED: Movie Instanc Detail: ' + req.params.id);
+};
+
+// Display create MovieInstrances w/ GET
+exports.movieInstance_create_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Movie Instance create GET');
+};
+
+// Handle movie instance creation with POST
+exports.movieInstance_create_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Movie Instance Create POST');
+};
+
+// Display delete MovieInstrances w/ GET
+exports.movieInstance_delete_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Movie Instance delete GET');
+};
+
+// Handle delete MovieInstrances w/ POST
+exports.movieInstance_delete_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Movie Instance delete Post');
+};
+
+// Display update MovieInstrances w/ GET
+exports.movieInstance_update_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Movie Instance update GET');
+};
+
+// Display update MovieInstrances w/ POST
+exports.movieInstance_update_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Movie Instance update POST');
+};
