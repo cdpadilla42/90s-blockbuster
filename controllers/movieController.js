e605b8fe1e5256ff148bf47f6e0944421ce7afef
Chris Padilla <cdpadilla42@gmail.com>
2020-04-04 10:52:51 -0500

    Add directors to movie form

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -2,24 +2,25 @@ const Movie = require('../models/movie');
 const Director = require('../models/director');
 const Genre = require('../models/genre');
 const MovieInstance = require('../models/movieinstance');
+const expressValidator = require('express-validator');
 
 const async = require('async');
 
 exports.index = (req, res) => {
   async.parallel(
     {
-      director_count: callback => {
+      director_count: (callback) => {
         Director.countDocuments({}, callback);
       },
-      genre_count: callback => {
+      genre_count: (callback) => {
         Genre.countDocuments({}, callback);
-      }
+      },
     },
     (err, results) => {
       res.render('index', {
         title: 'Local Blockbuster Home',
         error: err,
-        data: results
+        data: results,
       });
     }
   );
@@ -33,7 +34,7 @@ exports.movie_list = (req, res, next) => {
       if (err) return next(err);
       res.render('movie_list', {
         title: 'Movie List',
-        movie_list: list_movies
+        movie_list: list_movies,
       });
     });
 };
@@ -44,14 +45,112 @@ exports.movie_detail = (req, res) => {
 };
 
 // Display create movie GET
-exports.movie_create_get = (req, res) => {
-  res.send('NOT IMPLEMENTED: movie create GET');
+exports.movie_create_get = (req, res, next) => {
+  // Get all directors and genres for adding to our book
+  async.parallel(
+    {
+      directors: function (callback) {
+        Director.find(callback);
+      },
+      genres: function (callback) {
+        Genre.find(callback);
+      },
+    },
+    function (err, results) {
+      if (err) return next(err);
+      res.render('movie_form.hbs', {
+        title: 'Create new Movie',
+        directors: results.directors,
+        genres: results.genres,
+      });
+    }
+  );
 };
 
 // Handle create movie POST
-exports.movie_create_post = (req, res) => {
-  res.send('NOT IMPLEMENTED: movie create POST');
-};
+exports.movie_create_post = [
+  // Convert genre to array
+  (req, res, next) => {
+    if (!(req.body.genre instanceof Array)) {
+      if (typeof req.body.genre === 'undefined') {
+        req.body.genre = [];
+      } else {
+        req.body.genre = new Array(req.body.genre);
+      }
+      next();
+    }
+  },
+  expressValidator
+    .body('title', 'Title must not be empty')
+    .trim()
+    .isLength({ min: 1 }),
+  expressValidator
+    .body('director', 'Director must not be empty')
+    .trim()
+    .isLength({ min: 1 }),
+  expressValidator
+    .body('summary', 'Summary must not be empty')
+    .trim()
+    .isLength({ min: 1 }),
+  expressValidator
+    .body('isbn', 'isbn must not be empty')
+    .trim()
+    .isEmpty({ min: 1 }),
+
+  // Sanitize fields with wildcar
+  expressValidator.sanitizeBody('*').escape(),
+
+  // Process request after validation and sanitization
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    // Create a movie object with escaped and trimmed data
+    const movie = new Movie({
+      title: req.body.title,
+      director: req.body.director,
+      summary: req.body.summary,
+      isbn: req.body.isbn,
+      genre: req.body.genre,
+    });
+
+    if (!errors.isEmpty()) {
+      async.parallel(
+        {
+          directors: function (callback) {
+            Director.find(callback);
+          },
+          genres: function (callback) {
+            Genre.find(callback);
+          },
+        },
+        function (err, result) {
+          if (err) return next(err);
+
+          // Mark selected genres as checked
+          for (let i = 0; i < results.genres.length; i++) {
+            if (movie.genre.indexOf(results.genres[i]._id) > -1) {
+              results.genres[i].checked = 'true';
+            }
+          }
+
+          res.render('movie_form', {
+            title: 'Create Movie',
+            directors: results.directors,
+            genres: results.genres,
+            movie: movie,
+            errors: errors.array(),
+          });
+          return;
+        }
+      );
+    } else {
+      movie.save(function (err) {
+        if (err) return next(err);
+        res.redirect(movie.url);
+      });
+    }
+  },
+];
 
 // Display update movie GET
 exports.movie_update_get = (req, res) => {
